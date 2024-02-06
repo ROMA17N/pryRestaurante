9bfe199bba0c3cd3f1e4d526b5334004621b8f56
ROMA17N <victorrpl91@gmail.com>
2024-02-06 13:51:34 -0600

    Primeros cambios al proyecto (mi primera chambaaa !!)

---
+++ src/js/script1.js
@@ -0,0 +1,9 @@
+const location1 =      "New York, "      +      "NY";
+const location2="New York,"+"NY";
+const location3="New York,"      +"NY"
+
+console.log(location1);
+console.log(location2);
+console.log(location3)
+
+
